27d4bb6a65abf4844fc075d74c841a27ea3920f0
ashjan-senani <ashjan.al-senani@omantel.om>
2024-12-03 12:36:16 +0400

    Initial commit: Adding Finding Donors project files

---
+++ untitled.py
@@ -0,0 +1,8 @@
+# Import pandas library
+import pandas as pd
+
+# Load the census.csv file into a DataFrame
+data = pd.read_csv('census.csv')
+
+# Display the first 5 rows to check the data
+data.head()
